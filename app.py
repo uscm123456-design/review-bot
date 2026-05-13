51e821cb58a2f18c5aacfd54b1e65626647f28bf
uscm123456-design <uscm123456@gmail.com>
2026-05-14 00:55:10 +0900

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import time
 
 CLAUDE_API_KEY = st.secrets["CLAUDE_API_KEY"]
 
-APP_PASSWORD = st.secrets.get("APP_PASSWORD")
+APP_PASSWORD = st.secrets.get("uscm123456")
 
 if not APP_PASSWORD:
     st.error("APP_PASSWORD를 secrets에 추가해주세요.")
